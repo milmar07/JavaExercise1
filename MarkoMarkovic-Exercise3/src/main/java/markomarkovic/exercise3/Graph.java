667a2f425e4b0957b45accd5006e88c6af5ad763
Marko Markovic <activis95@yahoo.com>
2020-10-06 15:08:10 +0200

    Creating Exercuse3 with Graph

---
+++ MarkoMarkovic-Exercise3/src/main/java/markomarkovic/exercise3/Graph.java
@@ -0,0 +1,13 @@
+package markomarkovic.exercise3;
+
+/**
+ *
+ * @author Marko Markovic
+ */
+public class Graph {
+    
+    public static void main(String[] args){
+    
+    System.out.println("Hello");
+    }
+}
