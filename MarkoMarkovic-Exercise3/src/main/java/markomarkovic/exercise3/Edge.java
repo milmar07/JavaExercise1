3a75ba8da2d11572ddb498bb1c2edff3766d2b29
Marko Markovic <activis95@yahoo.com>
2020-10-06 18:16:28 +0200

    creating Edge class

---
+++ MarkoMarkovic-Exercise3/src/main/java/markomarkovic/exercise3/Edge.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package markomarkovic.exercise3;
+
+/**
+ *
+ * @author HAF
+ */
+class Edge {
+    public Vertex start;
+    public Vertex end;
+    public int weight;
+}
