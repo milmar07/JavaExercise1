e8c254b4dd80a6e6336414185d3245bb44f458c3
Marko Markovic <activis95@yahoo.com>
2020-10-15 16:22:02 +0200

    Update of errors for Exercise 4
    The previouse version had a problem because whenever your entry has any two words who are repeating the same amount, one of them is being forgoten about, the new version handling that error and pushing top 3 elements.

--- MarkoMarkovic-Exercise4/src/main/java/markomarkovic/exercise4/WordCountAndMultiSets.java
+++ MarkoMarkovic-Exercise4/src/main/java/markomarkovic/exercise4/WordCountAndMultiSets.java
@@ -1,12 +1,16 @@
 package markomarkovic.exercise4;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
 import com.google.common.collect.HashMultiset; 
 import com.google.common.collect.Multiset; 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -19,6 +23,19 @@ public class WordCountAndMultiSets {
     public WordCountAndMultiSets(){
         this.multiset = HashMultiset.create(); 
     }
+    
+    // sorting by value
+    public static <String, Integer extends Comparable <? super Integer> > Map <String, Integer> sortByValue(Map<String, Integer> map){
+        List<Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Entry.<String,Integer>comparingByValue().reversed());
+        
+        Map<String, Integer> result = new LinkedHashMap<>();
+        for(Entry<String, Integer> entry: list){
+                result.put(entry.getKey(), entry.getValue());
+        }
+        
+        return result;
+        }
     
     public static void main(String[] args){
         WordCountAndMultiSets firstBag = new WordCountAndMultiSets();
@@ -42,16 +59,18 @@ public class WordCountAndMultiSets {
         
         System.out.println("The most common 3 words: ");
         
-        TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>(Collections.reverseOrder());
+        Map<String, Integer> map = new HashMap<String, Integer>();
         
         for(Iterator<String> it = firstBag.multiset.iterator(); it.hasNext();){
             String nextLoad = new String();
             nextLoad = it.next();
-            treeMap.put(firstBag.multiset.count(nextLoad), nextLoad);    
+            map.put(nextLoad,firstBag.multiset.count(nextLoad)); 
         }
-        
-        TreeMap<Integer, String> myNewMap = treeMap.entrySet().stream().limit(3).collect(TreeMap::new, (m,e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
-        System.out.println(myNewMap);
+        //sorting the map by values
+        map = sortByValue(map);
+        // limiting map to only 3 elements
+        Map<String, Integer> topThree = map.entrySet().stream().limit(3).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (v1,v2) -> v1, LinkedHashMap::new));
+        System.out.println(topThree);
     }
 }
 
