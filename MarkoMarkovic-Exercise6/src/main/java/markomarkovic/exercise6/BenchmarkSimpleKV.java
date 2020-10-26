5afa58964f5221662142b487f52c1eabf77a7e77
Marko Markovic <activis95@yahoo.com>
2020-10-26 13:14:49 +0100

    new update
    not finished jet

--- MarkoMarkovic-Exercise6/src/main/java/markomarkovic/exercise6/BenchmarkSimpleKV.java
+++ MarkoMarkovic-Exercise6/src/main/java/markomarkovic/exercise6/BenchmarkSimpleKV.java
@@ -19,31 +19,34 @@ public class BenchmarkSimpleKV {
     
     public static void main(String[] args){
         Simple<String,Integer> kv1 = new SimpleKV();
-        BenchmarkSimpleKV bs = new BenchmarkSimpleKV();
-        int temp = 0;
         
         
-        Random rd = new Random();
         String alphabet = "ABCDEFGHIJLMNOPQRSTUVWXYZ";
-        StringBuilder stb = new StringBuilder();
-        
-        long startTime = System.nanoTime();
-        while (temp < bs.KEYPAIRS){
-            temp = kv1.getLenght();
-            int randomLenght = (int)(Math.random()*10+1);
-            for(int i = 0; i<randomLenght; i++){
-                int index = rd.nextInt(alphabet.length());
-                char randomChar = alphabet.charAt(index);
-                stb.append(randomChar);
+        for(int i=0; i<10;i++){
+            Random rd = new Random();
+            StringBuilder stb = new StringBuilder();
+            int temp = 0;
+            long startTime=0;
+            long endTime=0;
+            long totalTime=0;
+            
+            startTime = System.nanoTime();
+            while (temp < KEYPAIRS){
+                temp+=1;
+                for(int j = 0; j<30; j++){
+                    int index = rd.nextInt(alphabet.length());
+                    char randomChar = alphabet.charAt(index);
+                    stb.append(randomChar);
+            }
+            String randomString = stb.toString();
+            int randomNmb = rd.nextInt(10000);
+            kv1.put(randomString, randomNmb);
+            stb.setLength(0);
+            }
+            endTime = System.nanoTime();
+            totalTime = (endTime - startTime)/1000000L;
+            System.out.println("The total time in [" + i + "] try is : " + totalTime + " ms");
         }
-        String randomString = stb.toString();
-        int randomNmb = rd.nextInt();
-        kv1.put(randomString, randomNmb); 
-        }
-        long endtime = System.nanoTime();
-        long totalTime = (endtime - startTime)/1000000L;
-        System.out.println(totalTime);
-    
     
     
     }
